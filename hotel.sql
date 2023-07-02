61de55600437632fdfae8d9c48aa47744c7d4d61
RotsyMandrindra <128161749+RotsyMandrindra@users.noreply.github.com>
2023-07-02 18:19:00 +0300

    Update hotel.sql

--- hotel.sql
+++ hotel.sql
@@ -1,12 +1,15 @@
-CREATE DATABASE gestion_hotel;
---creation de la base de données gestion d'hotel
+DROP DATABASE IF EXISTS gestion_hotel;
 
-CREATE TABLE hotel(
+CREATE DATABASE gestion_hotel;
+
+CREATE TABLE hotel (
     id_hotel int primary key,
     nom varchar(50) not null,
     adresse varchar(250) not null,
     nombre_de_chambre int not null,
     telephone varchar(13) not null,
     email varchar(50) not null,
-    etoile int not null
+    etoile int not null,
+    id_receptionniste_hotel int,
+    foreign key (id_receptionniste_hotel) references receptionniste(id_receptionniste)
 );
