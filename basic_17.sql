e42bd7d2d14bc744ef3f3daa78e9070a284d5a9a
Lisa RASOANIRINA <127594297+rasoanirinamialisoa@users.noreply.github.com>
2023-07-12 14:25:08 +0300

    Delete basic_17.sql

--- basic_17.sql
+++
@@ -1,7 +0,0 @@
-SELECT h.*, COUNT(r.id_reservation) AS reservation_count
-FROM hotel h
-LEFT JOIN room r ON h.id_hotel = r.id_hotel
-LEFT JOIN reservation rs ON r.id_reservation = rs.id_reservation
-GROUP BY h.id_hotel
-ORDER BY reservation_count ASC
-LIMIT 1;
