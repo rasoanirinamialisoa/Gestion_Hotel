695145d26521f955c3f91e461b750422573b4989
Nantenaina <128221497+Nante1805@users.noreply.github.com>
2023-07-12 20:58:15 +0300

    Add files via upload

---
+++ Request/basic_16.sql
@@ -0,0 +1,7 @@
+SELECT h.*, COUNT(r.id_reservation) AS reservation_count
+FROM hotel h
+LEFT JOIN room r ON h.id_hotel = r.id_hotel
+LEFT JOIN reservation rs ON r.id_reservation = rs.id_reservation
+GROUP BY h.id_hotel
+ORDER BY reservation_count DESC
+LIMIT 1;
