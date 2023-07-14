37052f142f994f24739dfb745e933787f4c104ab
Lisa RASOANIRINA <127594297+rasoanirinamialisoa@users.noreply.github.com>
2023-07-15 00:06:09 +0300

    Update create_table.sql

--- Table/create_table.sql
+++ Table/create_table.sql
@@ -183,7 +183,7 @@ ALTER TABLE client ALTER COLUMN CIN TYPE VARCHAR(200);
 --change work_contact's type on vachar
 ALTER TABLE receptionist ALTER COLUMN work_contact TYPE VARCHAR(200);
 
---add column id_hotel and constraint in table receptionist
+-- Add id_hotel column and foreign key constraint to the receptionist table
 ALTER TABLE receptionist
 ADD COLUMN id_hotel INT,
 ADD CONSTRAINT fk_receptionist_hotel
