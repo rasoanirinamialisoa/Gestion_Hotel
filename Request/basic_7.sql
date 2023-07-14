93276ef494dea9f0ac62e019eba6ab22c231c2fb
RotsyMandrindra <128161749+RotsyMandrindra@users.noreply.github.com>
2023-07-14 16:15:20 +0300

    fix : Afficher la liste des clients qui n'ont pas encore payé en totalité leurs frais

--- Request/basic_7.sql
+++ Request/basic_7.sql
@@ -1,5 +1,5 @@
-SELECT c.*
+--Afficher la liste des clients qui n'ont pas encore payé en totalité leurs frais
+SELECT c.id_client, c.name
 FROM client c
-INNER JOIN payment p ON c.id_client = p.client_id
-GROUP BY c.id_client
-HAVING SUM(p.amount_paid) < SUM(p.amount_due);
+INNER JOIN payment p ON p.id_client = c.id_client
+WHERE p.total_amount_status = false;
