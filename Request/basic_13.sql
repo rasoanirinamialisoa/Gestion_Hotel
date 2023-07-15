f1c005c2cebd76180b163ec03bbeb49774a64cc6
Lisa RASOANIRINA <127594297+rasoanirinamialisoa@users.noreply.github.com>
2023-07-15 17:17:47 +0300

    Update basic_13.sql

--- Request/basic_13.sql
+++ Request/basic_13.sql
@@ -1,4 +1,5 @@
 --Afficher la liste des chambres dont la description correspond à des mots-clés données 
 SELECT *
 FROM room INNER JOIN room_features
- ON room_features.id_features = room.id_features WHERE mot_clé = true;
+ ON room_features.id_features = room.id_features 
+ WHERE sea_view = true AND VIP_category = true;
