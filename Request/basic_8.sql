d7d6e16bd028e21e21c67789475fe0055591e96f
RotsyMandrindra <128161749+RotsyMandrindra@users.noreply.github.com>
2023-07-15 14:16:43 +0000

    feat : add comment

--- Request/basic_8.sql
+++ Request/basic_8.sql
@@ -1,3 +1,4 @@
+--Afficher le total des payements reçus par mobile money
 SELECT SUM(p.amount_paid) AS total_mobile_money_payments
 FROM payment p
 INNER JOIN payment_method pm ON p.id_payment_method = pm.id_payment_method
