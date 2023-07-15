754f3b5e37a9b114d559fa7d852965a6723f8dc9
Lisa RASOANIRINA <127594297+rasoanirinamialisoa@users.noreply.github.com>
2023-07-15 17:23:24 +0300

    Update basic_3.sql

--- Request/basic_3.sql
+++ Request/basic_3.sql
@@ -1,5 +1,8 @@
 --Afficher la liste des réservations en commençant par la plus récente, pour un hotel donné
-SELECT * 
-FROM reservation
-WHERE id_hotel= id_hotel_données
-ORDER BY date_arrived DESC;
+
+SELECT r.*
+FROM reservation r
+INNER JOIN room rm ON r.id_reservation = rm.id_reservation
+INNER JOIN hotel h ON rm.id_hotel = h.id_hotel
+WHERE h.hotel_name = 'Akory hotel Majunga'
+ORDER BY r.date_arrived DESC;
