7713b80cf1ba06b86e957c8335ca2615109351d4
Lisa RASOANIRINA <127594297+rasoanirinamialisoa@users.noreply.github.com>
2023-07-15 17:32:22 +0300

    Update basic_4.sql

--- Request/basic_4.sql
+++ Request/basic_4.sql
@@ -1,6 +1,7 @@
---Afficher une liste des rooms d'un type donné (chambre simple, twin, vip...), dans un hotel donné
+--Afficher une liste des rooms d'un type donné (chambre simple, twin, vip...), dans un hotel donn
+
 SELECT room.id_room, room.room_type
 FROM room
 INNER JOIN hotel ON room.id_hotel = hotel.id_hotel
-WHERE hotel.hotel_name = hotel_name--this hotel name is a parametre it can be change
-AND room.room_type = room_type--this room type is parametre it can be change;
+WHERE hotel.hotel_name = 'Akory hotel Fianarantsoa'
+AND room.room_type = 'VIP';
