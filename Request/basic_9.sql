e9318f3bf97f1f7a8ca5cf091345a570af43b98c
Lisa RASOANIRINA <127594297+rasoanirinamialisoa@users.noreply.github.com>
2023-07-15 17:44:07 +0300

    Update basic_9.sql

--- Request/basic_9.sql
+++ Request/basic_9.sql
@@ -1,7 +1,8 @@
 --Afficher le nombre de réservations effectuées par un client donné durant une période donnée
+
 SELECT COUNT(*) AS reservation_count
 FROM reservation r
-INNER JOIN client c ON r.client_id = c.id_client
-WHERE c.name = 'Nantenaina'
-  AND r.date_arrived >= '<date_debut>'
-  AND r.leaving_date <= '<date_fin>';
+INNER JOIN client c ON r.id_client = c.id_client
+WHERE c.name = 'Lucille'
+  AND r.date_arrived >= '2023-01-26'
+  AND r.leaving_date <= '2023-06-09';
