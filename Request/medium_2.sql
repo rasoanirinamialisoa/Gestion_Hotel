275c8c41f9daf77d610c6ed9bfd6eb82fd05d587
Lisa RASOANIRINA <127594297+rasoanirinamialisoa@users.noreply.github.com>
2023-07-15 17:48:31 +0300

    Update medium_2.sql

--- Request/medium_2.sql
+++ Request/medium_2.sql
@@ -1,4 +1,7 @@
 --Afficher combien de fois le client a reservé dans nos hotels (tout hotel confondu)
-SELECT COUNT(*) AS nombre_de_reservations
-FROM reservation 
-WHERE id_client = 1;
+
+SELECT c.name, c.last_name, COUNT(r.id_reservation) AS total_reservations
+FROM client c
+INNER JOIN reservation r ON c.id_client = r.id_client
+WHERE c.id_client = 2
+GROUP BY c.name, c.last_name;
