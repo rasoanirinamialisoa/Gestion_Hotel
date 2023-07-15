df0d812b774d8509699a463fc385fef62e01c5ec
Lisa RASOANIRINA <127594297+rasoanirinamialisoa@users.noreply.github.com>
2023-07-15 18:00:02 +0300

    Update medium_5.sql

--- Request/medium_5.sql
+++ Request/medium_5.sql
@@ -1,8 +1,10 @@
 --Afficher la liste des chambres qui répondent à de multiples critères, par ex: y a le wifi, eau chaude, balcon,
 --jacuzzi, et qui peuvent contenir X personnes selon le besoin du client
-SELECT *
-FROM room
-INNER JOIN room_features ON room_features.id_features = room.id_features
-WHERE room_features.critere = true/false AND room_features.critere1 = 
-true/false;--with critere and critere1 can be change
---true/false is a choice you can choose true or false  
+
+SELECT r.*
+FROM room r
+INNER JOIN room_features rf ON r.id_features = rf.id_features
+WHERE rf.wifi_available = true
+  AND rf.hot_water = true
+  AND rf.mini_bar = true
+  AND r.capacity_room >= 2;
