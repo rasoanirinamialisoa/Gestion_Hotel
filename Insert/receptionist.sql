d462bfadee5435c33e938d43ad878280661e536d
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2023-08-03 18:01:17 +0300

    ajouter application receptionist

---
+++ Insert/receptionist.sql
@@ -0,0 +1,22 @@
+INSERT INTO receptionist (first_name, last_name, Password, email, work_contact)
+VALUES
+  ('Jayne', 'Bubbear', '6Hds7w89', 'jbubbear0@npr.org', '0322249507'),
+  ('Kaine', 'Balleine', '3LYQzEZ4', 'kballeine1@g.co', '0331719233'),
+  ('Reider', 'Quiddington', '2Af9l5i8', 'rquiddington2@a8.net', '0347748159'),
+  ('Justinn', 'Dorning', 'Jzhb2Y5K', 'jdorning3@altervista.org', '0345477641'),
+  ('Skelly', 'Henlon', 'FKi1O8B3', 'shenlon4@boston.com', '0343505690'),
+  ('Myrtice', 'Gentery', '5Kb4e1yh', 'mgentery5@army.mil', '0332960377'),
+  ('Maggy', 'Sneddon', 'Y5e3hfOW', 'msneddon6@google.de', '0320822161'),
+  ('Tristam', 'Debnam', 'F0Q1loUS', 'tdebnam7@godaddy.com', '0343437197'),
+  ('Jimmie', 'Dumingos', '5z4e2ZT3', 'jdumingos8@eepurl.com', '0342685998'),
+  ('Shanon', 'Whyke', 'sIZi09z3', 'swhyke9@linkedin.com', '0320720054'),
+  ('Jordan', 'Turner', 'R7lX3g6j', 'jturnera@goo.gl', '0321474731'),
+  ('Marcella', 'Rieger', '3g2WphD7a', 'mriegerb@symantec.com', '0334903053'),
+  ('Ole', 'Darree', '03OGr3m9z', 'odarreec@wikimedia.org', '0339265067'),
+  ('Antonina', 'Ham', 'G6SdrZYz', 'ahamd@hibu.com', '0349781865'),
+  ('Marielle', 'Ingham', '82X9tWKQgX', 'minghame@eepurl.com', '0348258103'),
+  ('Barclay', 'Veregan', '83F98hXe', 'bveregang@pbs.org', '0341916831'),
+  ('Lizette', 'Borkin', 'ZQd6fwun', 'lborkinh@51.la', '0338919461'),
+  ('Gwennie', 'Dumsday', '9fFSMYyEPB', 'gdumsdayi@over-blog.com', '0338412844'),
+  ('Maxie', 'Spohrmann', 'Vys9IFQ', 'mspohrmannj@phoca.cz', '0334636764'),
+  ('Kippy', 'Shipp', 'KJ5Qdny1', 'kshippk@merriam-webster.com', '0347123483');
