abdda02c5697848b8628cba413b11adeda75d8ad
Lisa RASOANIRINA <127594297+rasoanirinamialisoa@users.noreply.github.com>
2023-08-07 01:28:16 +0300

    Update basic_6.sql

--- Request/basic_6.sql
+++ Request/basic_6.sql
@@ -1,5 +1,6 @@
 -- Afficher la liste des réservations d'un client donné
 
-SELECT *
+SELECT reservation.*, name
 FROM reservation
-WHERE id_client = 1;
+INNER JOIN client ON reservation.id_client = client.id_client
+WHERE client.name = 'Daniel' OR client.name = 'Maria';
